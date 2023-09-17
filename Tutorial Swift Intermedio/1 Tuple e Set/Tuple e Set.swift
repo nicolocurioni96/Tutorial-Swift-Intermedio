c5a52219d26db2b4814752ce1e8b6c81c1e31e2d
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-17 23:12:13 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Intermedio/1 Tuple e Set/Tuple e Set.swift
@@ -0,0 +1,23 @@
+//
+//  Tuple e Set.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 17/09/23.
+//
+
+import SwiftUI
+
+struct Tuple_e_Set: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .padding()
+        }
+    }
+}
+
+struct Tuple_e_Set_Previews: PreviewProvider {
+    static var previews: some View {
+        Tuple_e_Set()
+    }
+}
