bfa8561a04725eec87dd6106bea5300f83c698b1
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-17 23:05:45 +0200

    Initial Commit

---
+++ Tutorial Swift Intermedio/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 17/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutorial Swift Intermedio/Tutorial_Swift_IntermedioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_Swift_IntermedioApp.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_Swift_IntermedioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
