f0354f4330dc5d410339a58d3ef655cc9f7a4378
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-19 22:04:42 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Intermedio/3 Le Funzioni/Funzioni.swift
@@ -0,0 +1,21 @@
+//
+//  Funzioni.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 19/09/23.
+//
+
+import SwiftUI
+
+struct Funzioni: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    Funzioni()
+}
