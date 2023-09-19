5dde5229f5e374766199a9c2d15f925c9e1be271
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-19 23:19:16 +0200

    Added `Dictionary` content to the tutorial

--- Tutorial Swift Intermedio/2 Dictionary/I Dictionary.swift
+++ Tutorial Swift Intermedio/2 Dictionary/I Dictionary.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct I_Dictionary: View {
+    /// `Dictionary`
+    
+    // Create a sample dictionary
+    var httpResponseMessages = [
+        200: "OK",
+        404: "File not found",
+        500: "Internal server error"]
+    
+    // Create an empty dictionary
+    var emptyDictionary: [String: String] = [:]
+    
+    init() {
+        // (Uncomment to test)
+        // Read and Change a value in the Dictionary
+        //print(httpResponseMessages[500])
+        //httpResponseMessages[500] = "Ops, something went wrong w/the server..."
+        //print(httpResponseMessages[500])
+        
+        for (statusCode, statusMessage) in httpResponseMessages {
+            print("⚠️ HTTP Statuses\n\nCode 👉🏻 \(statusCode)\nMessage 👉🏻 \(statusMessage)")
+        }
+    }
+    
     var body: some View {
         VStack {
-            Text("Hello, World!")
+            Text("I Dictionary")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundStyle(.orange)
                 .padding()
         }
     }
