fbd61adceb67118d33267067e67070edaad92db7
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-19 21:59:31 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Intermedio/2 Dictionary/I Dictionary.swift
@@ -0,0 +1,21 @@
+//
+//  I Dictionary.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 19/09/23.
+//
+
+import SwiftUI
+
+struct I_Dictionary: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    I_Dictionary()
+}
