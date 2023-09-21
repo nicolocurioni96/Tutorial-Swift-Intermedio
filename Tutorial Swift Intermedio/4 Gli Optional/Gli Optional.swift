b5631ebc0a37e29328b0d614c10c33520cc00e75
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-21 12:11:48 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Intermedio/4 Gli Optional/Gli Optional.swift
@@ -0,0 +1,21 @@
+//
+//  Gli Optional.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 21/09/23.
+//
+
+import SwiftUI
+
+struct Gli_Optional: View {
+    var body: some View {
+        VStack {
+            Text("Gli Optional")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    Gli_Optional()
+}
