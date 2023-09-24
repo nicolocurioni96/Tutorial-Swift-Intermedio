482e125eb7c33fab266dd7859e68a000d355c92f
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-24 15:26:14 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Intermedio/5 Classi e Strutture/Classi e Strutture.swift
@@ -0,0 +1,21 @@
+//
+//  Classi e Strutture.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 24/09/23.
+//
+
+import SwiftUI
+
+struct Classi_e_Strutture: View {
+    var body: some View {
+        VStack {
+            Text("Classi e Strutture")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    Classi_e_Strutture()
+}
