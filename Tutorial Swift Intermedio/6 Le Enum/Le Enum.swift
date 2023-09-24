227032cce0fa77e4156cde0522e622d7b8cc628d
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-24 16:02:25 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Intermedio/6 Le Enum/Le Enum.swift
@@ -0,0 +1,21 @@
+//
+//  Le Enum.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 24/09/23.
+//
+
+import SwiftUI
+
+struct Le_Enum: View {
+    var body: some View {
+        VStack {
+            Text("Le Enum")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    Le_Enum()
+}
