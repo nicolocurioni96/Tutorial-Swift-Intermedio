b54b90f5baec57df69765c0c3bda758512dd76ca
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-24 15:57:34 +0200

    Added `Classi & Strutture` content to the tutorial

--- Tutorial Swift Intermedio/5 Classi e Strutture/Classi e Strutture.swift
+++ Tutorial Swift Intermedio/5 Classi e Strutture/Classi e Strutture.swift
@@ -7,7 +7,34 @@
 
 import SwiftUI
 
+/// Struct (value type)
+struct Document {
+    var text: String
+}
+
+/// Classes (reference type)
+/*class Document {
+    var text: String
+    
+    init(text: String) {
+        self.text = text
+    }
+}*/
+
 struct Classi_e_Strutture: View {
+    /// `Classes` & `Structures`
+    
+    init() {
+        // Struct and Classes ops
+        var myDoc = Document(text: "I 💙 Apple")
+        var friendDoc = myDoc
+        
+        friendDoc.text = "I 💖 Google"
+        
+        print(friendDoc.text)
+        print(myDoc.text)
+    }
+    
     var body: some View {
         VStack {
             Text("Classi e Strutture")
