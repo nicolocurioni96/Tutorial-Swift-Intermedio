70f561c7b21beb2b425de012a235a7c321949fed
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-26 20:38:19 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Intermedio/7 Gli Switch/Gli Switch.swift
@@ -0,0 +1,27 @@
+//
+//  Gli Switch.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 26/09/23.
+//
+
+import SwiftUI
+
+struct Gli_Switch: View {
+    /// `Gli Switch`
+    
+    init() {
+        
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Gli Switch")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    Gli_Switch()
+}
