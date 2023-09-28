8499d7ea2b236caf15cb1ec6b34112ea020d2114
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-28 18:34:10 +0200

    Added `Switch` content to the tutorial

--- Tutorial Swift Intermedio/7 Gli Switch/Gli Switch.swift
+++ Tutorial Swift Intermedio/7 Gli Switch/Gli Switch.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct Gli_Switch: View {
-    /// `Gli Switch`
+    /// `Switch` statements
+    
+    // 1) Check a specific case
+    let weather = "abc"
+    
+    // 2) SE-0380 if-switch expressions
+    let score = 700
     
     init() {
+        // 1) Check a specific case
+        /*switch weather {
+        case "rain":
+            print("We need an umbrella!")
+        case "snow":
+            print("I love ❄️!")
+        case "sunny":
+            print("It's a beautiful day! 🌞")
+        default:
+            print("IDK")
+        }*/
         
+        // 2) SE-0380 if-switch expressions
+        let aResult = if score >= 500 { "Great job!" } else { "Failed" }
+        //print(aResult)
+        
+        let complexResult = switch score {
+        case 0...300: "Failed"
+        case 301...500: "Great job! ✅"
+        default: "Failed"
+        }
+        
+        print(complexResult)
     }
     
     var body: some View {
