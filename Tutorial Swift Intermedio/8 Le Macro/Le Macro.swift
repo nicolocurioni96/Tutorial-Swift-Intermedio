98752b2a3416fc798a9f49ba009a2112e47afa86
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-30 17:02:24 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Intermedio/8 Le Macro/Le Macro.swift
@@ -0,0 +1,27 @@
+//
+//  Le Macro.swift
+//  Tutorial Swift Intermedio
+//
+//  Created by Nicolò Curioni on 30/09/23.
+//
+
+import SwiftUI
+
+struct Le_Macro: View {
+    /// `Swift Macro`
+    
+    init() {
+        
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Swift Macro")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    Le_Macro()
+}
